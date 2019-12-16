7b20440d03a1200b6b88e0202d4300334896198f
jingachao <1254603835@qq.com>
2019-12-16 15:15:17 +0800

    第二次提交

---
+++ src/main/java/com/example/springbootitrip/SpringBootItripApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootitrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootItripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootItripApplication.class, args);
+    }
+
+}
